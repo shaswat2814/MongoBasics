2ae381d97dc0b4c457da0d89c3f9d5c91b88b5e3
shaswat2814 <shaswat2814@gmail.com>
2021-08-15 22:55:21 +0530

    Validation Errors

--- product.js
+++ product.js
@@ -17,38 +17,61 @@ const productSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required:true,
+        required: true,
         min: 0
     },
-    onSale:{
-        type:Boolean,
+    onSale: {
+        type: Boolean,
         default: false
     },
-    categories:[String],
-    qty:{
-        online:{
-            type:Number,
-            default:0
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
         },
-        inStore:{
-            type:Number,
-            default:0
+        inStore: {
+            type: Number,
+            default: 0
         }
+    },
+    size:{
+        type:String,
+        enum:['S','X','L']
     }
 });
 
 const Product = mongoose.model('Product', productSchema);
- 
-// const bike = new Product({ name: 'Mountain Bike', price: "Hello" });//the validation wwe check if it can change the value to desired format specified int he schema
-const bike = new Product({name:"Bike Helmet" ,price:"9.9",categories:['cycling','Biking']});//will be added to the database as the string will be parsed to a number easily
 
+// const bike = new Product({ name: 'Mountain Bike', price: "Hello" });//the validation wwe check if it can change the value to desired format specified int he schema
+// const bike = new Product({name:"Bike Helmet" ,price:"9.9",categories:['cycling','Biking']});//will be added to the database as the string will be parsed to a number easily
+
+// const bike = new Product({name:"Tire Pump",price:19.9,categories:["cycling"]});
+// bike.save()
+//     .then(data => {
+//         console.log("IT WORKED");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("ERROr!");
+//         console.log(err);
+//     })
+
+//updating a document against a validation 
+// Product.findOneAndUpdate({ name: "Tire Pump" }, { price: -100.1 }, { new: true ,runValidators:true})
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+const bike = new Product({name:"Tire Pump",price:19.9,categories:["cycling"],size:'XL'});
 
 bike.save()
     .then(data => {
-        console.log("IT WORKED");
         console.log(data);
     })
     .catch(err => {
-        console.log("ERROr!");
         console.log(err);
     })
